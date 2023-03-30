18b1b13457327976648f01f1620812c07712ab48
mtrivinom <101816868+mtrivinom@users.noreply.github.com>
2023-03-30 13:13:30 +0200

    Conexión DB

---
+++ DB/Conexion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DotNet_GenteFit.DB {
+    internal class DB {
+        
+        SqlConnection db;
+
+        public DB() {
+            this.db = new SqlConnection("Server=DESKTOP-VHM9FJ7\\SQLEXPRESS;Database=GentefitDatabase;User Id=Usuario;Password=;");
+        }
+
+        public bool conectar() {
+            try {
+                this.db.Open();
+                return true;
+            } catch(SqlException ex) { 
+                return false;
+            }
+        }
+
+        public void desconectar() {
+            this.db.Close();
+        }
+
+    }
+
+}
