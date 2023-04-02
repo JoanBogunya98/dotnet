a0e3d088571a08b6b15dfc606c57cf33661fb7cb
Pedro Gallardo <pgallardoco@uoc.edu>
2023-04-02 18:22:54 +0200

    context

--- DotNet_GenteFit.CapaDatos/GentefitDatabaseContext.cs
+++ DotNet_GenteFit.CapaDatos/GentefitDatabaseContext.cs
@@ -35,7 +35,6 @@ public partial class GentefitDatabaseContext : DbContext
     public virtual DbSet<Reserva> Reservas { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=GentefitDatabase;Integrated Security=True;Trust Server Certificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
